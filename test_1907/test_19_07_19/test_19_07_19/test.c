71c80acd4138ec4b62db86e659e1f1bdb0187d39
Luckyboy1024 <2819549538@qq.com>
2019-07-20 20:34:22 +0800

    提交2019年07月19日的代码

---
+++ test_1907/test_19_07_19/test_19_07_19/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+#if 0
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	int ret = rand() % 100 + 1;
+	int i = 0;
+	int j = 0;
+	return 0;
+}
+#endif
