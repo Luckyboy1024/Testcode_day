852467149e343434d36e6fd6bd3496736b0b53d7
Luckyboy1024 <2819549538@qq.com>
2019-08-01 16:02:43 +0800

    提交2019年07月29日的代码

---
+++ test_1907/test_19_07_29/test_19_07_29/test.c
@@ -0,0 +1,187 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
+
+#if 0
+switch
+
+{ ?
+	{ ?
+		{ ?  
+			goto error; 
+		}
+	}
+}
+error:
+?
+
+	{ ? 
+
+	{ 
+
+
+
+
+		{ 
+
+
+
+			{ ? 
+
+
+
+
+switch (variable) 
+{ 
+case value1 :  ?  
+	break; 
+case value2 :  ?  
+	break; 
+	? 
+default :   ?  
+			break; 
+}
+
+{ 
+
+
+	for (int x=0; x<N; x++) 
+	{ 
+		? 
+	}
+
+for (i=0; i<N; i++) 
+{ 
+	if (condition) 
+		DoSomething(); 
+	else 
+		DoOtherthing(); 
+}
+
+for (col=0; col<5; col++ ) 
+{ 
+	for (row=0; row<100; row++) 
+	{ 
+		sum = sum + a[row][col]; 
+	} 
+}
+
+for (row=0; row<100; row++) 
+{ 
+	for ( col=0; col<5; col++ ) 
+	{ 
+		sum = sum + a[row][col]; 
+	} 
+}
+
+class A 
+{ 
+public: 
+	void Func1(void); 
+	void Func2(void); 
+	… 
+private: 
+	int    i, j; 
+	float  x, y; 
+	… 
+}
+
+class A 
+{ 
+private: 
+	int    i, j; 
+	float  x, y; 
+	… 
+public: 
+	void Func1(void); 
+	void Func2(void); 
+	… 
+}
+
+
+// 空行 
+
+virtual CMatrix CMultiplyMatrix (CMatrix leftMatrix, 
+									CMatrix rightMatrix); 
+
+for (very_longer_initialization; 
+	very_longer_condition; 
+	very_longer_update) 
+{ 
+	dosomething(); 
+}
+
+{
+	?
+	{
+		?
+	}
+	?
+}
+{ 
+
+
+	{ 
+
+		 
+
+
+		{ 
+
+
+
+
+
+
+			{ 
+
+
+				? 
+
+
+			} 
+
+
+
+
+
+	} 
+
+		?
+	}
+while (condition)
+{
+	statement1;
+	// 空行 
+	if (condition)
+	{
+		statement2;
+	}
+	else
+	{
+		statement3;
+	}
+	// 空行 
+	statement4;
+}
+
+width;
+// 
+
+int width;
+// 宽度 
+int height; // 
+
+int height; // 高度 
+int depth;
+// 
+
+int depth;
+// 深度 
+
+#endif
