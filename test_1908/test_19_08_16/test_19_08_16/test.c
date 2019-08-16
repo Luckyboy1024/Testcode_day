fcf5608a78b9a28ec3c3eac623e496d2a41b33b2
Luckyboy1024 <2819549538@qq.com>
2019-08-16 17:35:10 +0800

    添加源文件Test_CPP.cpp，添加测试代码

--- test_1908/test_19_08_16/test_19_08_16/test.c
+++ test_1908/test_19_08_16/test_19_08_16/test.c
@@ -1,5 +1,94 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+
+
+
+#if 0
+#include <iostream.h>
+void output(int x); // 函数声明 
+void output(float x); // 函数声明 
+
+void output(int x)
+{
+	cout << "";
+}
+
+
+
+{ 
+	cout << " output int " << x << endl ; 
+} 
+
+void output( float x) 
+{ 
+
+	cout << " output float " << x << endl ; 
+} 
+
+void main(void) 
+{ 
+
+	int   x = 1; 
+
+	float y = 1.0; 
+
+	output(x);  
+
+	// output int 1 
+
+	output(y);  
+
+	// output float 1 
+
+	output(1);  
+
+	// output int 1 
+	// output(0.5); 
+
+	// error! ambiguous call, 
+
+
+	void output( int x) 
+	{ 
+
+		cout << " output int " << x << endl ; 
+	} 
+
+	void output( float x) 
+	{ 
+
+		cout << " output float " << x << endl ; 
+	} 
+
+	void main(void) 
+	{ 
+
+		int   x = 1;
+
+		float y = 1.0;
+
+		output(x);
+
+		// output int 1 
+
+		output(y);
+
+		// output float 1 
+
+		output(1);
+
+		// output int 1 
+		// output(0.5); 
+
+		// error! ambiguous call, 因为自动类型转换 
+
+		output(int(0.5)); // output int 0 
+
+		output(float(0.5));
+		// output float 0.5 
+	}
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -48,3 +137,5 @@ void Test(void)
 		delete a;
 	delete b;
 }
+
+#endif
