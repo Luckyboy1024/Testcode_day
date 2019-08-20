2e23d1b76337940884b07722c87992b13e31ee14
Luckyboy1024 <2819549538@qq.com>
2019-08-20 10:11:56 +0800

    提交2019年08月20日的代码

---
+++ test_1908/test_19_08_20/test_19_08_20/test.c
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	char a = 0xA5;
+	char b = ~a >> 5;
+	printf("%d\n", b);
+	return 0;
+}
+
+#if 0
+
+void Foo(int x, int y);
+inline void Foo(int x, int y) // inline与函数定义体放在一起 
+{
+	⋯
+}
+
+void Foo(int x, int y)
+{
+	⋯
+}
+
+
+class A
+{
+public:
+	void Foo(int x, int y) { ⋯ }		// 自动地成为内联函数 
+}
+
+// 
+
+
+// 头文件 
+class A
+{
+public:
+	void Foo(int x, int y)；
+}
+
+// 定义文件 
+inline void A::Foo(int x, int y)
+{
+	⋯
+}
+
+
+
+#endif
