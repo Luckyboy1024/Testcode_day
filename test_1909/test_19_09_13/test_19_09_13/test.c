59544753c064aed78cf91e5e7f52159883309b26
Luckyboy1024 <2819549538@qq.com>
2019-09-13 13:02:52 +0800

    提交2019年09月13日的代码

---
+++ test_1909/test_19_09_13/test_19_09_13/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+#if 0
+int main()
+{
+	char* str1 = "hello";
+	char* str2 = "boy";
+	char* n = strcat(*str1, *str2);
+	printf("%s\n", n);
+	return 0;
+}
+#endif
