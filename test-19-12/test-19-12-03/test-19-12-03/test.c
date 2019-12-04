2a685e00d5c34038383ca0cf6e8cadbf22ad2686
Luckyboy1024 <2819549538@qq.com>
2019-12-04 10:42:24 +0800

    20191204:提交代码：操20191203测试

---
+++ test-19-12/test-19-12-03/test-19-12-03/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+
+
+#if 0
+
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *env[])
+{
+	int i = 0;
+	for (i = 0; i < argc; ++i)
+	{
+		printf("argv[%d] = [%s]\n", i, argv[i]);
+	}
+	return 0;
+}
+
+#endif
