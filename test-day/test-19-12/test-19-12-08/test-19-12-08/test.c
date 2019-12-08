0e91385cbe015ba0fe3d8cc2f355a2e8f227c7bc
Luckyboy1024 <2819549538@qq.com>
2019-12-08 21:09:08 +0800

    20191208:代码提交:20191208 测试代码 —— 九宫格（未写完）

---
+++ test-day/test-19-12/test-19-12-08/test-19-12-08/test.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+// 项目——九宫格
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define ROW 9
+#define COL 9
+#define _row (ROW / 3)	// 3*3 块 行
+#define _col (COL / 3)	// 3*3 块 列
+
+
+void print(uint32_t* arr, uint32_t row, uint32_t col)
+{
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			printf("----");
+		}
+		printf("-\n");
+		for (int j = 0; j < col; ++j)
+		{
+			printf("|%2d ", *(arr + i*row + j));
+		}
+		printf("|\n");
+	}		
+	for (int j = 0; j < col; ++j)
+	{
+		printf("----");
+	}
+	printf("-\n");
+}
+
+void start()
+{
+	uint32_t arr[ROW][COL] = { 0 };
+	uint32_t row = ROW;
+	uint32_t col = COL;
+	do
+	{
+		print(arr, row, col);
+	} while (1);
+}
+
+int main()
+{
+	start();
+	return 0;
+}
+
